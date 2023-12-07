d73579caed796cf34f3bfc2be92598cb731a1417
Demyan Popov <popovdeman37@gmail.com>
2023-12-07 23:40:16 +0600

    add file Wallet.java

---
+++ src/model/Wallet.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Wallet {
+    private double amount;
+    public Wallet(int amount){
+        this.amount = amount;
+    }
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}
